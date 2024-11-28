1fa54cbf74bdc4bce096e337a14ba90d1dc230ac
yashdobariya-scaletech <yash.dobariya@scaletech.xyz>
2024-11-28 12:29:26 +0530

    tailwind css setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+// tailwind.config.cjs
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Includes Astro components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
