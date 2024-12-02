22bb2e1a951666ba519119c69e86dbd17b8d4c1d
yashdobariya-scaletech <yash.dobariya@scaletech.xyz>
2024-12-02 19:38:39 +0530

    feat: design feature,faq and footer section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Includes Astro components
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        scroll: "scroll 60s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
